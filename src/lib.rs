270fbef43779d426a5e415863cbb61e2cab00b82
koopa1338 <sinner1991@gmail.com>
2023-03-21 15:53:45 +0100

    moved impl block of error and fix message

--- src/lib.rs
+++ src/lib.rs
@@ -26,6 +26,21 @@ pub enum BuilderErr {
     PromptError,
 }
 
+impl std::fmt::Display for BuilderErr {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let message = match self {
+            BuilderErr::NoPrivileges => "builder has to be startet as root",
+            BuilderErr::KernelConfigMissing => "Missing .config file in /usr/src",
+            BuilderErr::LinkingFileError(msg) => msg,
+            BuilderErr::KernelBuildFail(msg) => msg,
+            BuilderErr::PromptError => "error setting up prompt for user input",
+        };
+        write!(f, "BuildErr: {}", message)
+    }
+}
+
+impl std::error::Error for BuilderErr {}
+
 #[derive(Clone, Debug)]
 struct VersionEntry {
     path: PathBuf,
@@ -212,18 +227,3 @@ impl<'conf> KernelBuilder<'conf> {
             .map_err(|_| BuilderErr::PromptError)
     }
 }
-
-impl std::fmt::Display for BuilderErr {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        let message = match self {
-            BuilderErr::NoPrivileges => "builder has to be startet as root",
-            BuilderErr::KernelConfigMissing => "Missing .config file in /usr/src",
-            BuilderErr::LinkingFileError(msg) => msg,
-            BuilderErr::KernelBuildFail(msg) => msg,
-            BuilderErr::PromptError => "error setting up prompt for user input",
-        };
-        write!(f, "NoPriviligesError: {}", message)
-    }
-}
-
-impl std::error::Error for BuilderErr {}
